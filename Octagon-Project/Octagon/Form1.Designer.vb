dd986481341d3d0d1864ac1e8e180ce79a343802
Kanezane <sgroi.andrea89@gmail.com>
2016-03-06 00:29:58 +0100

    Progetto Octagon

---
+++ Octagon-Project/Octagon/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.map = New System.Windows.Forms.PictureBox()
+        Me.spartan = New System.Windows.Forms.PictureBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.map, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.spartan, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'map
+        '
+        Me.map.Image = CType(resources.GetObject("map.Image"), System.Drawing.Image)
+        Me.map.Location = New System.Drawing.Point(-3, -4)
+        Me.map.Name = "map"
+        Me.map.Size = New System.Drawing.Size(450, 450)
+        Me.map.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.map.TabIndex = 0
+        Me.map.TabStop = False
+        '
+        'spartan
+        '
+        Me.spartan.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.spartan.Image = Global.WindowsApplication1.My.Resources.Resources.spartan
+        Me.spartan.Location = New System.Drawing.Point(196, 12)
+        Me.spartan.Name = "spartan"
+        Me.spartan.Size = New System.Drawing.Size(50, 50)
+        Me.spartan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.spartan.TabIndex = 1
+        Me.spartan.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(332, 410)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Blue
+        Me.Button1.Enabled = False
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Crimson
+        Me.Button1.Location = New System.Drawing.Point(99, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(10, 429)
+        Me.Button1.TabIndex = 4
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Blue
+        Me.Button2.Enabled = False
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Crimson
+        Me.Button2.Location = New System.Drawing.Point(216, 7)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(10, 429)
+        Me.Button2.TabIndex = 5
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Blue
+        Me.Button3.Enabled = False
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Crimson
+        Me.Button3.Location = New System.Drawing.Point(330, 7)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(10, 429)
+        Me.Button3.TabIndex = 6
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Blue
+        Me.Button4.Enabled = False
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.Crimson
+        Me.Button4.Location = New System.Drawing.Point(9, 106)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(421, 11)
+        Me.Button4.TabIndex = 7
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Blue
+        Me.Button5.Enabled = False
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.ForeColor = System.Drawing.Color.Crimson
+        Me.Button5.Location = New System.Drawing.Point(11, 216)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(421, 11)
+        Me.Button5.TabIndex = 8
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Blue
+        Me.Button6.Enabled = False
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.ForeColor = System.Drawing.Color.Crimson
+        Me.Button6.Location = New System.Drawing.Point(7, 327)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(421, 11)
+        Me.Button6.TabIndex = 9
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 442)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.spartan)
+        Me.Controls.Add(Me.map)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.map, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.spartan, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents map As PictureBox
+    Friend WithEvents spartan As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Octagon-Project/Octagon/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Dim angle As Integer = 0
+    Dim p1 As Image = Image.FromFile(getClassPath() & "\Resources\spartan1.png")
+    Dim p2 As Image = Image.FromFile(getClassPath() & "\Resources\spartan2.png")
+    Dim p3 As Image = Image.FromFile(getClassPath() & "\Resources\spartan3.png")
+    Dim p4 As Image = Image.FromFile(getClassPath() & "\Resources\spartan4.png")
+    Dim p5 As Image = Image.FromFile(getClassPath() & "\Resources\spartan5.png")
+    Dim p6 As Image = Image.FromFile(getClassPath() & "\Resources\spartan6.png")
+    Dim p7 As Image = Image.FromFile(getClassPath() & "\Resources\spartan7.png")
+    Dim p8 As Image = Image.FromFile(getClassPath() & "\Resources\spartan8.png")
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        Select Case (e.KeyCode)
+            Case Keys.Left
+                angle = (angle - 45) Mod 360
+            Case Keys.Right
+                angle = (angle + 45) Mod 360
+        End Select
+
+        Select Case (angle)
+            Case 45, -315
+                spartan.Image = p2
+                spartan.Location = setPoint(3, 1)
+            Case 90, -270
+                spartan.Image = p3
+                spartan.Location = setPoint(3, 2)
+                spartan.Left += spartan.Width
+            Case 135, -225
+                spartan.Image = p4
+                spartan.Location = setPoint(3, 3)
+            Case 180, -180
+                spartan.Image = p5
+                spartan.Location = setPoint(2, 3)
+                spartan.Top += spartan.Height
+            Case 225, -135
+                spartan.Image = p6
+                spartan.Location = setPoint(1, 3)
+            Case 270, -90
+                spartan.Image = p7
+                spartan.Location = setPoint(1, 2)
+                spartan.Left -= spartan.Width
+            Case 315, -45
+                spartan.Image = p8
+                spartan.Location = setPoint(1, 1)
+            Case 0, 360
+                spartan.Image = p1
+                spartan.Location = setPoint(2, 1)
+                spartan.Top -= spartan.Height
+            Case Else
+                spartan.Image = p1
+                spartan.Location = setPoint(1, 1)
+        End Select
+
+        TextBox2.Text = "angle: " & angle
+        spartan.Refresh()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Focus()
+        spartan.Parent = map
+        spartan.BackColor = Color.Transparent
+    End Sub
+
+    Function setPoint(mx As Integer, my As Integer)
+        Dim x As Integer = ((Me.Width / 4) * mx) - (spartan.Width / 1.4)
+        Dim y As Integer = ((Me.Height / 4) * my) - (spartan.Height / 1.4)
+        Return New Point(x, y)
+    End Function
+
+    Function getClassPath()
+        Dim binFolder() As String = Split(My.Application.Info.DirectoryPath.ToString, "\")
+        Dim classPath As String = ""
+        For cont = 0 To binFolder.Count - 3
+            If cont = 0 Then
+                classPath = classPath & binFolder(cont)
+            Else
+                classPath = classPath & "\" & binFolder(cont)
+            End If
+        Next
+        Return classPath
+    End Function
+
+    Private Sub spartan_Click(sender As Object, e As EventArgs) Handles spartan.Click
+
+    End Sub
+
+    Private Sub map_Click(sender As Object, e As EventArgs) Handles map.Click
+
+    End Sub
+End Class
