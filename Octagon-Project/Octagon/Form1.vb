e15e6aef29f8361c7d6095ae27852e7462bae0a4
Polocchia <polocchia@gmail.com>
2016-03-07 00:35:06 +0100

    Migliorate alcune cosette

--- Octagon-Project/Octagon/Form1.vb
+++ Octagon-Project/Octagon/Form1.vb
@@ -9,8 +9,8 @@
     Dim p6 As Image = Image.FromFile(findClassPath.find & "\Resources\spartan6.png")
     Dim p7 As Image = Image.FromFile(findClassPath.find & "\Resources\spartan7.png")
     Dim p8 As Image = Image.FromFile(findClassPath.find & "\Resources\spartan8.png")
-    Dim x As Double = 0
-    Dim y As Double = 0
+    Dim x As Double
+    Dim y As Double
     Dim numx As Double = 0
     Dim numy As Double = 0
 
@@ -22,57 +22,59 @@
             'Case Keys.Right
             '    angle = (angle + 45) Mod 360
             Case Keys.Left
-                angle -= 10 Mod 360
-                x = Me.Width / 3 * Math.Cos(getRadiansFromDegree(angle))
-                y = Me.Height / 3 * Math.Sin(getRadiansFromDegree(angle))
+                angle -= 22.5 Mod 360
+                x = ((map.Width / 2.5) * Math.Cos(getRadiansFromDegree(angle))) - spartan.Width / 2
+                y = ((map.Height / 2.5) * Math.Sin(getRadiansFromDegree(angle))) - spartan.Height / 2
             Case Keys.Right
-                angle += 10 Mod 360
-                x = Me.Width / 3 * Math.Cos(getRadiansFromDegree(angle))
-                y = Me.Height / 3 * Math.Sin(getRadiansFromDegree(angle))
+                angle += 22.5 Mod 360
+                x = ((map.Width / 2.5) * Math.Cos(getRadiansFromDegree(angle))) - spartan.Width / 2
+                y = ((map.Height / 2.5) * Math.Sin(getRadiansFromDegree(angle))) - spartan.Height / 2
         End Select
 
         spartan.Image = p1
-        spartan.Location() = New Point(x, y)
+        spartan.Location() = New Point(x + Me.Width / 2, y + (Me.Height / 2) - (spartan.Height / 2))
 
+        If angle < 0 Then angle = 360 - angle
         Select Case (angle Mod 360)
-            Case >= 45 < 90, > -270 <= -315
-                spartan.Image= p2
-                TextBox1.Text="case 1"
-                'spartan.Location = setPoint(3, 1)
-            Case >= 90 < 135, > -225 <= -270
-                spartan.Image = p3
-                'spartan.Location = setPoint(3, 2)
-                'spartan.Left += spartan.Width
-                TextBox1.Text = "case 2"
-            Case >= 135 < 180, > -180 <= -225
-                spartan.Image = p4
-                spartan.Location = setPoint(3, 3)
-                TextBox1.Text = "case 3"
-            Case >= 180 < 225, > -135 <= -180
-                spartan.Image = p5
-                'spartan.Location = setPoint(2, 3)
-                'spartan.Top += spartan.Height
-                TextBox1.Text = "case 4"
-            Case >= 225 < 270, > -90 <= -135
-                spartan.Image = p6
-                'spartan.Location = setPoint(1, 3)
-                TextBox1.Text = "case 5"
-            Case >= 270 < 315, > -45 <= -90
-                spartan.Image = p7
-                'spartan.Location = setPoint(1, 2)
-                'spartan.Left -= spartan.Width
-                TextBox1.Text = "case 6"
-            Case >= 315 < 360, > 0 <= -45
-                spartan.Image = p8
-                'spartan.Location = setPoint(1, 1)
-                TextBox1.Text = "case 7"
-            Case >= 0 < 45, >= -315 < -360
+            Case 0 To 44 ', -315 To -359
                 spartan.Image = p1
                 'spartan.Location = setPoint(2, 1)
                 'spartan.Top -= spartan.Height
+                TextBox1.Text = "case 1"
+            Case 45 To 89 ', -270 To -314
+                spartan.Image = p2
+                TextBox1.Text = "case 2"
+            'spartan.Location = setPoint(3, 1)
+            Case 90 To 134 ', -225 To -269
+                spartan.Image = p3
+                'spartan.Location = setPoint(3, 2)
+                'spartan.Left += spartan.Width
+                TextBox1.Text = "case 3"
+            Case 135 To 179 ', -180 To -224
+                spartan.Image = p4
+                'spartan.Location = setPoint(3, 3)
+                TextBox1.Text = "case 4"
+            Case 180 To 224 ', -135 To -179
+                spartan.Image = p5
+                'spartan.Location = setPoint(2, 3)
+                'spartan.Top += spartan.Height
+                TextBox1.Text = "case 5"
+            Case 225 To 269 ', -90 To -134
+                spartan.Image = p6
+                'spartan.Location = setPoint(1, 3)
+                TextBox1.Text = "case 6"
+            Case 270 To 314 ', -45 To -89
+                spartan.Image = p7
+                'spartan.Location = setPoint(1, 2)
+                'spartan.Left -= spartan.Width
+                TextBox1.Text = "case 7"
+            Case 315 To 359 ', 0 To -44
+                spartan.Image = p8
+                'spartan.Location = setPoint(1, 1)
                 TextBox1.Text = "case 8"
-            Case Else
-                spartan.Image = p1
+
+                'Case Else
+                'spartan.Image = p1
                 'spartan.Location = setPoint(1, 1)
         End Select
 
@@ -89,7 +91,7 @@
     End Function
 
     Private Function getRadiansFromDegree(degree As Double) As Double
-        Dim radians As Double = (degree * Math.PI) / 180
+        Dim radians As Double = (degree * Math.PI) / 180.0
         Return radians
     End Function
 
