3fdd3b12af2e11fd235ee29134e0a1b4c2d6e391
Polocchia <polocchia@gmail.com>
2016-03-07 21:52:21 +0100

    Rotazione dello spartan in base alla posizione del cursore

--- Octagon-Project/Octagon/Form1.Designer.vb
+++ Octagon-Project/Octagon/Form1.Designer.vb
@@ -22,17 +22,23 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.map = New System.Windows.Forms.PictureBox()
         Me.spartan = New System.Windows.Forms.PictureBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         CType(Me.map, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.spartan, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'map
         '
+        Me.map.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.map.Image = CType(resources.GetObject("map.Image"), System.Drawing.Image)
         Me.map.Location = New System.Drawing.Point(-3, -4)
         Me.map.Name = "map"
@@ -43,7 +49,8 @@ Partial Class Form1
         '
         'spartan
         '
-        Me.spartan.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.spartan.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.spartan.Image = Global.WindowsApplication1.My.Resources.Resources.spartan1
         Me.spartan.Location = New System.Drawing.Point(196, 12)
         Me.spartan.Name = "spartan"
@@ -73,15 +80,34 @@ Partial Class Form1
         Me.TextBox1.Size = New System.Drawing.Size(62, 20)
         Me.TextBox1.TabIndex = 4
         '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.White
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.ForeColor = System.Drawing.Color.White
+        Me.TextBox3.Location = New System.Drawing.Point(171, 296)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(442, 442)
+        Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.spartan)
         Me.Controls.Add(Me.map)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.map, System.ComponentModel.ISupportInitialize).EndInit()
@@ -95,4 +121,6 @@ Partial Class Form1
     Friend WithEvents spartan As PictureBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Timer1 As Timer
 End Class
--- Octagon-Project/Octagon/Form1.vb
+++ Octagon-Project/Octagon/Form1.vb
@@ -29,54 +29,62 @@
                 angle += 22.5 Mod 360
                 x = ((map.Width / 2.5) * Math.Cos(getRadiansFromDegree(angle))) - spartan.Width / 2
                 y = ((map.Height / 2.5) * Math.Sin(getRadiansFromDegree(angle))) - spartan.Height / 2
+            Case Keys.W
+                spartan.Top -= 10
+            Case Keys.S
+                spartan.Top += 10
+            Case Keys.A
+                spartan.Left -= 10
+            Case Keys.D
+                spartan.Left += 10
         End Select
 
-        spartan.Image = p1
-        spartan.Location() = New Point(x + Me.Width / 2, y + (Me.Height / 2) - (spartan.Height / 2))
+        ' spartan.Image = p1
+        'spartan.Location() = New Point(x + Me.Width / 2, y + (Me.Height / 2) - (spartan.Height / 2))
 
         If angle < 0 Then angle = 360 - angle
-        Select Case (angle Mod 360)
-            Case 0 To 44 ', -315 To -359
-                spartan.Image = p1
-                'spartan.Location = setPoint(2, 1)
-                'spartan.Top -= spartan.Height
-                TextBox1.Text = "case 1"
-            Case 45 To 89 ', -270 To -314
-                spartan.Image = p2
-                TextBox1.Text = "case 2"
-            'spartan.Location = setPoint(3, 1)
-            Case 90 To 134 ', -225 To -269
-                spartan.Image = p3
-                'spartan.Location = setPoint(3, 2)
-                'spartan.Left += spartan.Width
-                TextBox1.Text = "case 3"
-            Case 135 To 179 ', -180 To -224
-                spartan.Image = p4
-                'spartan.Location = setPoint(3, 3)
-                TextBox1.Text = "case 4"
-            Case 180 To 224 ', -135 To -179
-                spartan.Image = p5
-                'spartan.Location = setPoint(2, 3)
-                'spartan.Top += spartan.Height
-                TextBox1.Text = "case 5"
-            Case 225 To 269 ', -90 To -134
-                spartan.Image = p6
-                'spartan.Location = setPoint(1, 3)
-                TextBox1.Text = "case 6"
-            Case 270 To 314 ', -45 To -89
-                spartan.Image = p7
-                'spartan.Location = setPoint(1, 2)
-                'spartan.Left -= spartan.Width
-                TextBox1.Text = "case 7"
-            Case 315 To 359 ', 0 To -44
-                spartan.Image = p8
-                'spartan.Location = setPoint(1, 1)
-                TextBox1.Text = "case 8"
+        'Select Case (angle Mod 360)
+        '    Case 0 To 44 ', -315 To -359
+        '        spartan.Image = p1
+        '        'spartan.Location = setPoint(2, 1)
+        '        'spartan.Top -= spartan.Height
+        '        TextBox1.Text = "case 1"
+        '    Case 45 To 89 ', -270 To -314
+        '        spartan.Image = p2
+        '        TextBox1.Text = "case 2"
+        '    'spartan.Location = setPoint(3, 1)
+        '    Case 90 To 134 ', -225 To -269
+        '        spartan.Image = p3
+        '        'spartan.Location = setPoint(3, 2)
+        '        'spartan.Left += spartan.Width
+        '        TextBox1.Text = "case 3"
+        '    Case 135 To 179 ', -180 To -224
+        '        spartan.Image = p4
+        '        'spartan.Location = setPoint(3, 3)
+        '        TextBox1.Text = "case 4"
+        '    Case 180 To 224 ', -135 To -179
+        '        spartan.Image = p5
+        '        'spartan.Location = setPoint(2, 3)
+        '        'spartan.Top += spartan.Height
+        '        TextBox1.Text = "case 5"
+        '    Case 225 To 269 ', -90 To -134
+        '        spartan.Image = p6
+        '        'spartan.Location = setPoint(1, 3)
+        '        TextBox1.Text = "case 6"
+        '    Case 270 To 314 ', -45 To -89
+        '        spartan.Image = p7
+        '        'spartan.Location = setPoint(1, 2)
+        '        'spartan.Left -= spartan.Width
+        '        TextBox1.Text = "case 7"
+        '    Case 315 To 359 ', 0 To -44
+        '        spartan.Image = p8
+        '        'spartan.Location = setPoint(1, 1)
+        '        TextBox1.Text = "case 8"
 
-                'Case Else
-                'spartan.Image = p1
-                'spartan.Location = setPoint(1, 1)
-        End Select
+        '        'Case Else
+        '        'spartan.Image = p1
+        '        'spartan.Location = setPoint(1, 1)
+        'End Select
 
         TextBox2.Text = "angle: " & angle Mod 360
         'spartan.Left += spartan.Width / 7
@@ -99,5 +107,32 @@
         Me.Focus()
         spartan.Parent = map
         spartan.BackColor = Color.Transparent
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim cx As Integer = Cursor.Position.X
+        Dim cy As Integer = Cursor.Position.Y
+        Dim cc As String = cx & "," & cy
+        TextBox3.Text = cc
+
+        If Cursor.Position.Y < (Me.Top + spartan.Top) And Cursor.Position.X > (Me.Left + spartan.Left) And Cursor.Position.X < (Me.Left + spartan.Left + spartan.Width) Then
+            spartan.Image = p1
+        ElseIf Cursor.Position.Y < (Me.Top + spartan.Top) And Cursor.Position.X > (Me.Left + spartan.Left + spartan.Width) Then
+            spartan.Image = p2
+        ElseIf Cursor.Position.Y > (Me.Top + spartan.Top) And Cursor.Position.Y < (Me.Top + spartan.Top + spartan.Height) And Cursor.Position.X > (Me.Left + spartan.Left + spartan.Width)
+            spartan.Image = p3
+        ElseIf Cursor.Position.Y > (Me.Top + spartan.Top) And Cursor.Position.X > (Me.Left + spartan.Left + spartan.Width)
+            spartan.Image = p4
+        ElseIf Cursor.Position.Y > (Me.Top + spartan.Top) And Cursor.Position.X > (Me.Left + spartan.Left) And Cursor.Position.X < (Me.Left + spartan.Left + spartan.Width)
+            spartan.Image = p5
+        ElseIf Cursor.Position.Y > (Me.Top + spartan.Top + spartan.Height) And Cursor.Position.X < (Me.Left + spartan.Left)
+            spartan.Image = p6
+        ElseIf Cursor.Position.Y > (Me.Top + spartan.Top) And Cursor.Position.Y < (Me.Top + spartan.Top + spartan.Height) And Cursor.Position.X < (Me.Left + spartan.Left)
+            spartan.Image = p7
+        ElseIf Cursor.Position.Y < (Me.Top + spartan.Top) And Cursor.Position.X < (Me.Left + spartan.Left)
+            spartan.Image = p8
+        End If
+ 
     End Sub
 End Class
