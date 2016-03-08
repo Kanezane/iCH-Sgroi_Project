1fe804bf7a65df2db450b8f1f8a4c018b1f9b96f
Polocchia <polocchia@gmail.com>
2016-03-08 23:15:21 +0100

    Forse ora va

---
+++ OctagonC/OctagonC/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.spartan = new System.Windows.Forms.PictureBox();
+            this.map = new System.Windows.Forms.PictureBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.spartan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.BackColor = System.Drawing.Color.White;
+            this.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextBox3.Enabled = false;
+            this.TextBox3.ForeColor = System.Drawing.Color.White;
+            this.TextBox3.Location = new System.Drawing.Point(174, 300);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.ReadOnly = true;
+            this.TextBox3.Size = new System.Drawing.Size(100, 20);
+            this.TextBox3.TabIndex = 10;
+            this.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Enabled = false;
+            this.TextBox1.Location = new System.Drawing.Point(187, 258);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(62, 20);
+            this.TextBox1.TabIndex = 9;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.BackColor = System.Drawing.Color.White;
+            this.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextBox2.Enabled = false;
+            this.TextBox2.ForeColor = System.Drawing.Color.White;
+            this.TextBox2.Location = new System.Drawing.Point(174, 215);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.ReadOnly = true;
+            this.TextBox2.Size = new System.Drawing.Size(100, 20);
+            this.TextBox2.TabIndex = 8;
+            this.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // spartan
+            // 
+            this.spartan.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spartan.BackColor = System.Drawing.SystemColors.Control;
+            this.spartan.Image = ((System.Drawing.Image)(resources.GetObject("spartan.Image")));
+            this.spartan.Location = new System.Drawing.Point(199, 16);
+            this.spartan.Name = "spartan";
+            this.spartan.Size = new System.Drawing.Size(50, 50);
+            this.spartan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.spartan.TabIndex = 7;
+            this.spartan.TabStop = false;
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Image = ((System.Drawing.Image)(resources.GetObject("map.Image")));
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.Name = "map";
+            this.map.Size = new System.Drawing.Size(450, 450);
+            this.map.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.map.TabIndex = 6;
+            this.map.TabStop = false;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 10;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.spartan);
+            this.Controls.Add(this.map);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ReadKey);
+            ((System.ComponentModel.ISupportInitialize)(this.spartan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.PictureBox spartan;
+        internal System.Windows.Forms.PictureBox map;
+        public System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ OctagonC/OctagonC/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        double angle = 0;
+        //FindClassPath findClassPath = new FindClassPath();
+
+        Image p1 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan1.png");
+        Image p2 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan2.png");
+        Image p3 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan3.png");
+        Image p4 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan4.png");
+        Image p5 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan5.png");
+        Image p6 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan6.png");
+        Image p7 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan7.png");
+        Image p8 = Image.FromFile(Environment.CurrentDirectory + "\\Resources\\spartan8.png");
+        double x;
+        double y;
+        //double numx = 0;
+        //double numy = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            map.Controls.Add(spartan);
+            spartan.BackColor = Color.Transparent;
+            this.Focus();
+            Timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        void ReadKey(object sender, KeyEventArgs e)
+        {
+            switch ((e.KeyCode))
+            {
+                
+                case Keys.Left:
+                    angle -= 22.5 % 360;
+                    x = ((map.Width / 2.5) * Math.Cos(getRadiansFromDegree(angle-90))) - spartan.Width / 2;
+                    y = ((map.Height / 2.5) * Math.Sin(getRadiansFromDegree(angle-90)));
+                    
+                    spartan.Location = new Point(Convert.ToInt16(x + Width / 2), Convert.ToInt16(y + (Height / 2) - (spartan.Height / 2)));
+                    break;
+                case Keys.Right:
+                    angle += 22.5 % 360;
+                    x = ((map.Width / 2.5) * Math.Cos(getRadiansFromDegree(angle-90))) - spartan.Width / 2;
+                    y = ((map.Height / 2.5) * Math.Sin(getRadiansFromDegree(angle-90)));
+
+                    spartan.Location = new Point(Convert.ToInt16(x + Width / 2), Convert.ToInt16(y + (Height / 2) - (spartan.Height / 2)));
+                    break;
+                /*
+                case Keys.W:
+                    spartan.Top -= 10;
+                    break;
+                case Keys.S:
+                    spartan.Top += 10;
+                    break;
+                case Keys.A:
+                    spartan.Left -= 10;
+                    break;
+                case Keys.D:
+                    spartan.Left += 10;
+                    break;
+                    */
+            }
+            if (angle < 0)
+                angle = 360 - angle;
+            /*Select Case (angle Mod 360)
+                Case 0 To 44 ', -315 To -359
+                    spartan.Image = p1
+                    'spartan.Location = setPoint(2, 1)
+                    'spartan.Top -= spartan.Height
+                    TextBox1.Text = "case 1"
+                Case 45 To 89 ', -270 To -314
+                    spartan.Image = p2
+                    TextBox1.Text = "case 2"
+                'spartan.Location = setPoint(3, 1)
+                Case 90 To 134 ', -225 To -269
+                    spartan.Image = p3
+                    'spartan.Location = setPoint(3, 2)
+                    'spartan.Left += spartan.Width
+                    TextBox1.Text = "case 3"
+                Case 135 To 179 ', -180 To -224
+                    spartan.Image = p4
+                    'spartan.Location = setPoint(3, 3)
+                    TextBox1.Text = "case 4"
+                Case 180 To 224 ', -135 To -179
+                    spartan.Image = p5
+                    'spartan.Location = setPoint(2, 3)
+                    'spartan.Top += spartan.Height
+                    TextBox1.Text = "case 5"
+                Case 225 To 269 ', -90 To -134
+                    spartan.Image = p6
+                    'spartan.Location = setPoint(1, 3)
+                    TextBox1.Text = "case 6"
+                Case 270 To 314 ', -45 To -89
+                    spartan.Image = p7
+                    'spartan.Location = setPoint(1, 2)
+                    'spartan.Left -= spartan.Width
+                    TextBox1.Text = "case 7"
+                Case 315 To 359 ', 0 To -44
+                    spartan.Image = p8
+                    'spartan.Location = setPoint(1, 1)
+                    TextBox1.Text = "case 8"
+
+                    'Case Else
+                    'spartan.Image = p1
+                    'spartan.Location = setPoint(1, 1)
+            End Select
+            */
+
+            TextBox2.Text = "angle: " + angle % 360;
+            spartan.Refresh();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            int cx = Cursor.Position.X;
+            int cy = Cursor.Position.Y;
+            string cc = cx + "," + cy;
+            TextBox3.Text = cc;
+            
+            if (Cursor.Position.Y < (this.Top + spartan.Top) 
+                & Cursor.Position.X > (this.Left + spartan.Left)                     // Compreso tra la X iniziale e finale dello spartan
+                & Cursor.Position.X < (this.Left + spartan.Left + spartan.Width))    // Compreso tra la X iniziale e finale dello spartan
+            {spartan.Image = p1;}
+
+            else if (Cursor.Position.Y < (this.Top + spartan.Top) 
+                & Cursor.Position.X > (this.Left + spartan.Left + spartan.Width))
+            {spartan.Image = p2;}
+
+            else if (Cursor.Position.Y > (this.Top + spartan.Top)                    // Compreso tra la Y iniziale e finale dello spartan
+                & Cursor.Position.Y < (this.Top + spartan.Top + spartan.Height)      // Compreso tra la Y iniziale e finale dello spartan
+                & Cursor.Position.X > (this.Left + spartan.Left + spartan.Width))
+            {spartan.Image = p3;}
+
+            else if (Cursor.Position.Y > (this.Top + spartan.Top) 
+                & Cursor.Position.X > (this.Left + spartan.Left + spartan.Width))
+            {spartan.Image = p4;}
+
+            else if (Cursor.Position.Y > (this.Top + spartan.Top+spartan.Height) 
+                & Cursor.Position.X > (this.Left + spartan.Left)                     // Compreso tra la X iniziale e finale dello spartan
+                & Cursor.Position.X < (this.Left + spartan.Left + spartan.Width))    // Compreso tra la X iniziale e finale dello spartan
+            {spartan.Image = p5;}
+
+            else if (Cursor.Position.Y > (this.Top + spartan.Top + spartan.Height) 
+                & Cursor.Position.X < (this.Left + spartan.Left))
+            {spartan.Image = p6;}
+
+            else if (Cursor.Position.Y > (this.Top + spartan.Top)                    // Compreso tra la Y iniziale e finale dello spartan
+                & Cursor.Position.Y < (this.Top + spartan.Top + spartan.Height)      // Compreso tra la Y iniziale e finale dello spartan
+                & Cursor.Position.X < (this.Left + spartan.Left))
+            {spartan.Image = p7;}
+
+            else if (Cursor.Position.Y < (this.Top + spartan.Top) 
+                & Cursor.Position.X < (this.Left + spartan.Left))
+            {spartan.Image = p8;}
+            
+        }
+        private object setPoint(int mx, int my)
+        {
+            int x = Convert.ToInt16(((this.Width / 4) * mx) - (spartan.Width / 1.4));
+            int y = Convert.ToInt16(((this.Height / 4) * my) - (spartan.Height / 1.4));
+            return new Point(x, y);
+        }
+        
+        private double getRadiansFromDegree(double degree)
+        {
+            double radians = (degree * Math.PI) / 180.0;
+            return radians;
+            
+        }
+
+    }
+}
