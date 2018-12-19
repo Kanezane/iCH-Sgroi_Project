dd1a0a7a62c15f97c0ce487ada004b8ea457b10e
Kanezane <sgroi.andrea89@gmail.com>
2018-12-19 19:42:06 +0100

    New Chat Project

---
+++ Chat/src/main/java/it/ichsugoroi/zexirioshin/main/Core.java
@@ -0,0 +1,8 @@
+package it.ichsugoroi.zexirioshin.main;
+
+public class Core {
+
+    public static void main(String[] argv) {
+        System.out.println("Questa è la classe principale! :D");
+    }
+}
